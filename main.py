9b0afd4717ef8d1c5e0418dba23d3fd588e93124
Francesca Rossi <rfrancesca2203@gmail.com>
2025-12-01 15:58:54 +0100

    Fix CORS

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ubiquitous-paprenjak-a1a6d1.netlify.app/"],
+    allow_origins=[
+        "https://ubiquitous-paprenjak-a1a6d1.netlify.app",
+        "http://ubiquitous-paprenjak-a1a6d1.netlify.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=False,
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
